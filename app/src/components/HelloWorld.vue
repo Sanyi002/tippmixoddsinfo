57dce702ab98e90335bc7fb3f9128558beaabc1d
Sandor Szabo <sanyi002@gmail.com>
2019-08-15 21:08:37 +0200

    add bootstrap to the project and set auto import to base scss files

--- app/src/components/HelloWorld.vue
+++ app/src/components/HelloWorld.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
+    <div class="alert alert-success">Megy ez a szar!</div>
     <p>
       For a guide and recipes on how to configure / customize this project,<br>
       check out the
